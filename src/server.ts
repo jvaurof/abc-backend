d440e0f2b23060808ba4e29884b20d6a03d85aad
jvaurof <jvaurof@gmail.com>
2022-04-25 12:28:05 -0300

    instalação do cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata"
 import express from 'express'
+import cors from 'cors'
 import './database'
 
 const app = express()
+app.use(cors())
 
 app.listen(3333, ()=> console.log('server is runing'))
